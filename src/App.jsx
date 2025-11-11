ca344cedbd315406722ce4541edbfb8cc7a62d05
nikookin1122 <nikolilia@icloud.com>
2025-11-11 15:56:46 +0100

    feat: Banner & Container created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Home from './pages/Home/Home'
+import Banner from './components/Banner/Banner'
+import Layout from './layouts/Layout/Layout'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route element={<Layout/>}>
+        <Route path='/' element={<Home/>}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/layouts/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import s from './Layout.module.css';
+import Banner from '../../components/Banner/Banner';
+import { Outlet } from 'react-router';
+
+
+const Layout = () => {
+    return(
+        <>
+        <Banner/>
+        <Outlet />
+        </>
+    )
+}
+
+
+export default Layout;
