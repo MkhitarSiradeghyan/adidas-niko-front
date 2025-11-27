de12f32259f2db6f6156a251e62bc28c0ba3c9b5
nikookin1122 <nikolilia@icloud.com>
2025-11-27 20:56:55 +0100

    feat: Shopping card

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import s from "./Home.module.sass"
-import Hero from '../../components/Hero/Hero'
-import Dressstyle from '../../components/Dressstyle/Dressstyle'
-import Subscribe from '../../components/Subscribe/Subscribe'
+import ProductCard from '../../components/ProductCard/ProductCard'
 
 const Home = () => {
   return (
-    <Subscribe/>
-
+    <ProductCard/>
+      
   )
 }
 
